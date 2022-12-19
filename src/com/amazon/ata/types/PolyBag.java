c69595a83b24924847caef56c89b59a1391aa1f8
Ralph2 <academy4@talentamp.com>
2022-12-19 08:59:48 +0800

    Finished

--- src/com/amazon/ata/types/PolyBag.java
+++ src/com/amazon/ata/types/PolyBag.java
@@ -1,20 +1,27 @@
 package com.amazon.ata.types;
 
 import java.math.BigDecimal;
+
 public class PolyBag extends Packaging {
     private double mass;
     private BigDecimal volume;
+
+
     /**
      * Instantiates a new Packaging object.
      *
      * @param material - the Material of the package
-     * @param volume   - the Volume of the package
      */
     public PolyBag(Material material, BigDecimal volume) {
         super(material);
         this.volume = volume;
+
+
     }
 
+    public BigDecimal getVolume() {
+        return volume;
+    }
 
     @Override
     public boolean canFitItem(Item item) {
@@ -24,11 +31,13 @@ public class PolyBag extends Packaging {
         BigDecimal itemVolume = itemHeight.multiply(itemLength).multiply(itemWidth);
         return volume.compareTo(itemVolume) == 0;
     }
+
     @Override
     public BigDecimal getMass() {
         double v = 0.6;
         double vVolume = volume.doubleValue();
         mass = Math.ceil(Math.sqrt(vVolume) * v);
+
         return BigDecimal.valueOf(mass);
     }
 }
